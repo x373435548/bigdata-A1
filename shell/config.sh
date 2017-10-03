270326e54ae8c55bc195149da9493ac6d2b19ffa
waltzcat <sruying@umich.edu>
2017-10-03 12:39:59 -0400

    t

--- shell/config.sh
+++ shell/config.sh
@@ -1,4 +1,9 @@
 #！ /bin/bash
 echo "export JAVA_HOME=/proj/michigan-bigdata-PG0/jdk1.8.0_144
 export PATH=\${JAVA_HOME}/bin:\$PATH" >> ~/.bashrc
-source ~/.bashrc
+
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
+sudo apt-get install vim-gtk
