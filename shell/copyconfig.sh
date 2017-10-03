9f3eb166cf4d1ae5aef4552a607ea6fededd809e
waltzcat <sruying@umich.edu>
2017-10-03 15:42:14 -0400

    ff

---
+++ shell/copyconfig.sh
@@ -0,0 +1,6 @@
+#！ /bin/bash
+cd ~
+cp bigdata-A1/xml/core-site.xml hadoop-2.7.4/etc/hadoop/
+cp bigdata-A1/xml/hdfs-site.xml hadoop-2.7.4/etc/hadoop/
+cp bigdata-A1/xml/slaves hadoop-2.7.4/etc/hadoop/
+exit 0
